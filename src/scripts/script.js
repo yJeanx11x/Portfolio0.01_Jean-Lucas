815d083c34d4055788761d7fa38b016846ed50c7
Jean Lucas <j3anlucas010@gmail.com>
2024-04-11 19:31:12 -0300

    Criação Dos Contatos

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,5 +8,7 @@ sumidao.reveal('.foto',{  rotate:{x:15,y:0,z:0},duration:1500 })
 sumidao.reveal('.sobre',{  rotate:{x:0,y:15,z:0},duration:1500 })
 sumidao.reveal('.perfil',{  rotate:{x:0,y:100,z:0},duration:2000 })
 sumidao.reveal('.Projetos',{  rotate:{x:0,y:100,z:0},duration:2600 })
+sumidao.reveal('.contatos',{  rotate:{x:100,y:5,z:0},duration:2600 })
 
 VanillaTilt.init(document.querySelectorAll(".card"));
+
